feff19d79f18a80395ae435aedb5628ccaefc919
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2024-09-04 18:44:16 +0530

    Normalize line endings

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from path;
+import mongoose from "mongoose";
+import multer from "multer";
+import helmet from 'helmet';
+import { fileURLToPath } from 'url';
+import morgan from "morgan";
+
+// CONFIGURATIONS
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit:'30mb' , extended:true}));
+app.use(bodyParser.urlencoded({limit:'30mb' , extended:true}));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,(request,response)=>{
+    console.log(`App running on port ${PORT}`)
+});
