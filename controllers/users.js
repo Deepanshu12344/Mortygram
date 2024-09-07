c3c93604c07f3dde866484adb9fdac4fb58b6533
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2024-09-07 12:14:19 +0530

    User Routes Complete

--- controllers/users.js
+++ controllers/users.js
@@ -33,6 +33,19 @@ export const getUserFriends= async(request,response)=>{
 
 export const addRemoveFriend= async(request,response)=>{
     try {
+        const {id,friendId} = req.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+        
+        if(user.friends.includes(friendId)){
+            user.friends = user.friends.filter((id)=>id!==friendId);
+            friend.friends = friend.friends.filter((id)=>id!==id);
+        }else{
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+        await user.save();
+        await friend.save();
         
     } catch (error) {
         console.log(error);
