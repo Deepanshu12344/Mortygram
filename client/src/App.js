a331b2d1cc1f985fbf9c755d310accaedec820ca
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2024-09-17 19:31:33 +0530

    starting frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginPage } from './pages/loginPage';
+import { HomePage } from './pages/homePage';
+import { ProfilePage } from './pages/profilePage';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/profile/:id' element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
