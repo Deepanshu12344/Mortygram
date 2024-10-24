25dc7cca3382917f6a964f9468cb49acc1d58176
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2024-10-24 18:47:19 +0530

    Navbar created successfully

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,27 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoginPage } from './pages/loginPage';
 import { HomePage } from './pages/homePage';
 import { ProfilePage } from './pages/profilePage';
+import { useMemo } from 'react';
+import { useSelector }from 'react-redux';
+import {themeSettings} from './theme.js'
+import {createTheme} from '@mui/material/styles';
+import { CssBaseline,ThemeProvider } from '@mui/material';
+
 
 function App() {
+  const mode = useSelector((state)=>state.mode);
+  const theme = useMemo(()=>createTheme(themeSettings(mode)),[mode]);
   return (
     <div className="App">
       <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> 
         <Routes>
           <Route path='/' element={<LoginPage />} />
           <Route path='/home' element={<HomePage />} />
           <Route path='/profile/:id' element={<ProfilePage />} />
         </Routes>
+      </ThemeProvider>
       </BrowserRouter>
     </div>
   );
